dcb38633d654f87f2ec79600c95bf027c2f4c5e9
AnkitAnkushe <ankitankushe@gmail.com>
2022-09-03 17:41:47 +0530

    today page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import {gapi} from 'gapi-script'
 import { useEffect } from 'react';
 import Home_BL from './components/Home_BL/Home_BL';
+import Today from './components/todayPage/Today';
 
 const clientId = "779029402943-c8c6rrt9rkfpf60it2qqv31agcgq8i55.apps.googleusercontent.com"
 
@@ -19,7 +20,8 @@ function App() {
   return (
     <div className="App">
      {/* <Navbar/> */}
-     <Home_BL/>
+     {/* <Home_BL/> */}
+     <Today/>
     </div>
   );
 }
---
+++ frontend/src/components/todayPage/Today.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Today = () => {
+
+  return (
+    <div style={{justifyContent:"center"}}>
+      <h1>Today's Inspiration 😎</h1>
+      <img src="./today1.png" alt="today_photo" />
+      <h1>4 September 2022</h1>
+      <img src="./today2.png" alt="today_photo" />
+      <h1>3 September 2022</h1>
+      <img src="./today3.png" alt="today_photo" />
+      <h1>2 September 2022</h1>
+      <img src="./today4.png" alt="today_photo" />
+    </div>
+  )
+}
+
+export default Today
